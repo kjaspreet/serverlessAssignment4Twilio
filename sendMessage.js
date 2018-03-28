1d78c51cb943a2111644288af8c1c5b0001386e0
kjaspreet <jaspreet.mca2011@gmail.com>
2018-03-28 02:33:01 -0800

    commit

--- sendMessage.js
+++ sendMessage.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const client = require('twilio')(
-  '<Your TWILIO ACCOUNT SID>',
-  '<YOUR TWILIO AUTH TOKEN>'
+  'AC86cbf57ff93572e3f1fe468f5b6235f3',
+  '6871ddfc7c5e3aeb727fc09c4eb950c5'
   );
 
 module.exports.sendMessage = (event, context, callback) => {
   client.messages.create({
   from: '+16042452855',
-  to: '<YOUR CELL NUMBER>',
+  to: '+16045124582',
   body: "New file is added to your S3 bucket"
 }).then((messsage) => console.log(message.sid));
 };
